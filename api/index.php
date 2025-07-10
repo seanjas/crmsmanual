b1ad533be6b44415011d69699ea79e5832fe52a3
unknown <seanjas>
2025-07-10 14:08:47 +0800

    forth commit

---
+++ api/index.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
